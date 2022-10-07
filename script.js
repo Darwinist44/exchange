a1afc60874ceceea9722577b2f8efdaba651d773
Darwinist44 <balazs.labbancz04@gmail.com>
2022-10-07 11:58:01 +0200

    teszt

---
+++ script.js
@@ -0,0 +1,29 @@
+const createHTML = (text) => {
+    let template = document.createElement("template");
+    template.innerHTML = text;
+    return template.content;
+}
+
+let symbols1 = document.getElementById('symbols1');
+let symbols2 = document.getElementById('symbols2');
+
+const symbols = () =>{
+    fetch("https://api.exchangerate.host/symbols", {
+            method: 'GET'
+        })
+            .then(res => res.json())
+            .then(data => {
+                
+                Object.keys(data.symbols).forEach((symbol) => {
+                        symbols1.appendChild(createHTML(`<option value="${symbol}">${symbol}</option>`));
+                        symbols2.appendChild(createHTML(`<option value="${symbol}">${symbol}</option>`));;
+               }) 
+            });
+}
+
+window.addEventListener('load', symbols);
+
+symbols1.addEventListener('change', (event) =>{
+    let selected = event.target.value;
+    console.log(selected);
+})
